5f829a49ff801bc2c77798eda0141f4295b36a69
Tyler <19tylermalone94@gmail.com>
2024-12-04 12:53:06 -0700

    clean up

--- app/src/main/java/aoc/days/Day4.java
+++ app/src/main/java/aoc/days/Day4.java
@@ -65,9 +65,7 @@ public class Day4 implements Day {
         int count = 0;
         for (int i = 1; i < M - 1; ++i) {
             for (int j = 1; j < N - 1; ++j) {
-                if (isX(i, j)) {
-                    ++count;
-                }
+                count += isX(i, j) ? 1 : 0;
             }
         }
         return count;
