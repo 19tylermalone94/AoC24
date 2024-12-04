33daa85cab2cd21b6882b934a04f629577fc89f6
Tyler <19tylermalone94@gmail.com>
2024-12-04 12:18:08 -0700

    day 4 part 1 clean up

--- app/src/main/java/aoc/days/Day4.java
+++ app/src/main/java/aoc/days/Day4.java
@@ -2,93 +2,69 @@ package aoc.days;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import aoc.util.Readers;
 
 public class Day4 implements Day {
 
-    Pattern frontPattern = Pattern.compile("XMAS");
-    Pattern backPattern = Pattern.compile("SAMX");
+    static final String word = "XMAS";
+    static final int[] di = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static final int[] dj = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    char[][] grid;
+    int M;
+    int N;
+
+    private void init(String data) throws IOException{
+        grid = Arrays.stream(data.split("\n"))
+                     .map(String::toCharArray)
+                     .toArray(char[][]::new);
+        M = grid.length;
+        N = grid[0].length;
+    }
 
     @Override
     public void run(String filename) throws IOException {
         String data = Readers.fileToString(filename);
-        int xmasCount = part1(data);
-        System.out.println(xmasCount);
-        System.out.println(part2(data));
+        System.out.printf("part 1: %d\npart 2: %d\n", part1(data), part2(data));
     }
 
-    int part1(String data) {
-        String[] rows = getRows(data);
-        String[] columns = getColumns(rows);
-        String[] leftDiagonals = getDiagonals(rows, true);
-        String[] rightDiagonals = getDiagonals(rows, false);
-        return countXMASOccurrences(rows) + countXMASOccurrences(columns) 
-            + countXMASOccurrences(leftDiagonals) + countXMASOccurrences(rightDiagonals);
+    int part1(String data) throws IOException {
+        init(data);
+        return IntStream.range(0, M * N)
+                        .map(i -> checkDirections(i / N, i % N))
+                        .sum();
     }
 
-    private int countXMASOccurrences(String[] data) {
-        return Arrays.stream(data)
-                     .mapToInt(this::countXMASOccurrences)
-                     .sum();
+    int checkDirections(int i, int j) {
+        return IntStream.range(0, 8)
+                        .map(d -> isFound(i, j, d) ? 1 : 0)
+                        .sum();
     }
 
-    private int countXMASOccurrences(String string) {
-        return (int) (frontPattern.matcher(string).results().count() + backPattern.matcher(string).results().count());
-
-    }
-
-    private String[] getRows(String data) {
-        return data.trim().split("\n");
-    }
-
-    private String[] getColumns(String[] rows) {
-        return IntStream.range(0, rows[0].length())
-                        .mapToObj(i -> Arrays.stream(rows)
-                                             .map(row -> row.substring(i, i + 1))
-                                             .collect(Collectors.joining()))
-                        .toArray(String[]::new);
-    }
-
-    private String[] getDiagonals(String[] rows, boolean left) {
-        int numDiags = rows[0].length() + rows.length - 1;
-        String[] diags = new String[numDiags];
-        for (int row = 0; row < numDiags; ++row) {
-            diags[row] = getDiag(row, rows, left);
-            diags[numDiags - row - 1] = getDiag(numDiags - row - 1, rows, left);
+    boolean isFound(int i, int j, int d) {
+        int k = 0;
+        while (i >= 0 && i < M && j >= 0 && j < N && k < word.length()) {
+            if (grid[i][j] != word.charAt(k)) {
+                return false;
+            }
+            i += di[d];
+            j += dj[d];
+            k++;
         }
-        return diags;
+        return k == word.length();
     }
 
-    private String getDiag(int row, String[] rows, boolean left) {
-        StringBuilder sb = new StringBuilder();
-        int i = row;
-        int j = left ? 0 : rows[0].length() - 1;
-        while (i >= rows.length) {
-            i--;
-            j += left ? 1 : -1;
-        }
-        while (i >= 0 && j < rows[0].length() && j >= 0) {
-            sb.append(rows[i--].charAt(left ? j++ : j--));
-        }
-        return sb.toString();
-    }
-
-    int part2(String data) {
+    int part2(String data) throws IOException {
+        init(data);
         int count = 0;
-        char[][] d = Arrays.stream(data.split("\n"))
-                           .map(String::toCharArray)
-                           .toArray(char[][]::new);
-        for (int i = 1; i < d.length - 1; ++i) {
-            for (int j = 1; j < d[0].length - 1; ++j) {
-                if (d[i][j] == 'A') {
-                    char topLeft = d[i - 1][j - 1];
-                    char topRight = d[i - 1][j + 1];
-                    char bottomLeft = d[i + 1][j - 1];
-                    char bottomRight = d[i + 1][j + 1];
+        for (int i = 1; i < M - 1; ++i) {
+            for (int j = 1; j < N - 1; ++j) {
+                if (grid[i][j] == 'A') {
+                    char topLeft = grid[i - 1][j - 1];
+                    char topRight = grid[i - 1][j + 1];
+                    char bottomLeft = grid[i + 1][j - 1];
+                    char bottomRight = grid[i + 1][j + 1];
                     if (isX(topLeft, topRight, bottomLeft, bottomRight)) {
                         ++count;
                     }
--- app/src/test/java/aoc/days/TestDay4.java
+++ app/src/test/java/aoc/days/TestDay4.java
@@ -1,12 +1,15 @@
 package aoc.days;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestDay4 {
 
-   String sample = """
+   final String sample1 = """
                 MMMSXXMASM
                 MSAMXMSMSA
                 AMXSXMAAMM
@@ -18,6 +21,20 @@ public class TestDay4 {
                 MAMMMXMMMM
                 MXMXAXMASX
                     """;
+
+    final String sample2 = """
+                .M.S......
+                ..A..MSMS.
+                .M.S.MAA..
+                ..A.ASMSM.
+                .M.S.M....
+                ..........
+                S.S.S.S.S.
+                .A.A.A.A..
+                M.M.M.M.M.
+                ..........
+                    """;
+
     Day4 day4;
 
     @BeforeEach
@@ -26,9 +43,16 @@ public class TestDay4 {
     }
 
     @Test
-    public void testPart1() {
-        long expected = 18;
-        long actual = day4.part1(sample);
+    public void testPart1() throws IOException {
+        int expected = 18;
+        int actual = day4.part1(sample1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPart2() throws IOException {
+        int expected = 9;
+        int actual = day4.part2(sample2);
         assertEquals(expected, actual);
     }
     
